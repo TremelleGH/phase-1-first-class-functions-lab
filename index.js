2a010db470dc121ebc72bfd78fbd2f7c1ceb2d14
Tremelle Ancrum <tancrum1996@gmail.com>
2022-05-17 16:57:13 -0400

    commit tree a828f9be139bdaf1b404d58bc854158bc97bec94

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers) {
+    return ['Antonia', 'Nuru']
+}
+
+const returnLastTwoDrivers = function(drivers) {
+    return ['Amari', 'Mo']
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+let createFareMultiplier = function(integer) {
+    return function(fare) {
+        return integer * fare
+    }
+}
+
+const fareDoubler = function(fare) {
+    return fare * 2
+}
+
+const fareTripler = function(fare) {
+    return fare * 3
+}
+
+function selectDifferentDrivers(drivers, driverOptions) {
+    return driverOptions(drivers)
+}
